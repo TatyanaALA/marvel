f1fea4288e1e70cbddadb9941c24d8cbd381347b
Tatyana <tanya_r@list.ru>
2024-03-04 17:08:15 +0500

    getting data about a random character

---
+++ src/services/MarvelServices.js
@@ -0,0 +1,37 @@
+
+class MarvelService {
+  _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+  _apiKey = 'apikey=46279e3b25d4652e4fa0a24da10216ea';
+
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+  }
+
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    return res.data.results.map(this._transformCharacter);
+  }
+
+  getCharacter = async (id) => {
+    const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    return this._transformCharacter(res.data.results[0]);
+  }
+
+  _transformCharacter = (char) => {
+    return{
+      name: char.name,
+      description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+      thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+      hompage: char.urls[0].url,
+      wiki: char.urls[1].url
+    }
+  }
+}
+
+export default MarvelService;
